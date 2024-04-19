e3f772c54910fba28b6e5a1efe7ead8075fac419
aarshil8 <aarshiljd@gmail.com>
2024-04-19 15:47:07 +0530

    Another creative landing page with react js

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,51 @@
+import "./Hero.css";
+import arrow from "../../assets/arrow.png";
+import play from "../../assets/play.png";
+import pause from "../../assets/pause.png";
+
+const Hero = ({
+  heroData,
+  setHeroCount,
+  heroCount,
+  setPlayStatus,
+  playStatus,
+}) => {
+  return (
+    <div className="hero">
+      <div className="hero-text">
+        <p>{heroData.text1}</p>
+        <p>{heroData.text2}</p>
+      </div>
+      <div className="hero-explore">
+        <p>Explore the features</p>
+        <img src={arrow} alt="" />
+      </div>
+      <div className="hero-dot-play">
+        <ul className="hero-dots">
+          <li
+            onClick={() => setHeroCount(0)}
+            className={heroCount === 0 ? "hero-dot orange" : "hero-dot"}
+          ></li>
+          <li
+            onClick={() => setHeroCount(1)}
+            className={heroCount === 1 ? "hero-dot orange" : "hero-dot"}
+          ></li>
+          <li
+            onClick={() => setHeroCount(2)}
+            className={heroCount === 2 ? "hero-dot orange" : "hero-dot"}
+          ></li>
+        </ul>
+        <div className="hero-play">
+          <img
+            onClick={() => setPlayStatus(!playStatus)}
+            src={playStatus ? pause : play}
+            alt=""
+          />
+          <p>See the video</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
